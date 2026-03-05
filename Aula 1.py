34f623add6aa6d8364b7e9a55dbdc23bf303cf37
4ndreTW <andrelucastw2@gmail.com>
2026-03-05 16:42:24 -0300

    Update Aula 1.py

--- Aula 1.py
+++ Aula 1.py
@@ -1,8 +1,10 @@
+import pandas
 import pyautogui
 import time
+tabela = pandas.read_csv("produtos.csv")
 # pyautogui.click -> clica
 # pyautogui.write -> escreve um texto
-# pyautogui.press -> aperta uma tecla
+# p'yautogui.press -> aperta uma tecla
 pyautogui.PAUSE = 0.5
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login" 
 # pyautogui.hotkey ->  aperta um atalho (hotkey)
@@ -18,9 +20,45 @@ pyautogui.press("tab")
 pyautogui.write("Lucas//@1512")
 pyautogui.press("tab")
 pyautogui.press("enter")
+
 time.sleep(2)
+for linha in tabela.index:
+    
+    # codigo
+    pyautogui.click(x=707, y=294)
+    codigo = str(tabela.loc[linha, "codigo"])
+    pyautogui.write(codigo)
+    # marca
+    marca = str(tabela.loc[linha, "marca"])
+    pyautogui.press("tab")
+    pyautogui.write(marca)
+    # tipo
+    tipo = str(tabela.loc[linha, "tipo"])
+    pyautogui.press("tab")
+    pyautogui.write(tipo)
+    # categoria
+    categoria = str(tabela.loc[linha, "categoria"])
+    pyautogui.press("tab")
+    pyautogui.write(categoria)
+    # preço
+    preco = str(tabela.loc[linha, "preco_unitario"])
+    pyautogui.press("tab")
+    pyautogui.write(preco)
+    # custo
+    custo = str(tabela.loc[linha, "custo"])
+    pyautogui.press("tab")
+    pyautogui.write(custo)
+    # obs
+    obs = str(tabela.loc[linha, "obs"])
+    pyautogui.press("tab")
+    if obs != "nan":
+        pyautogui.write(obs)
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    # scroll
+    pyautogui.scroll(5000)
 
-
+-------------------------------------------
 
 import time
 import pyautogui
